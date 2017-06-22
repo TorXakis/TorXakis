467ef406123f1cf3e1d93deb67e7afbfa6a3711e
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-06-22 10:24:09 +0200

    Changed extensions of log files to '.log'

--- test/examps/stimulusresponse/TestStimulusResponse.bat
+++ test/examps/stimulusresponse/TestStimulusResponse.bat
@@ -8,33 +8,33 @@ echo ------- Start StimulusResponse Test 1
 start /min java -cp %EXAMPS% StimulusResponse
 call %TORXAKIS% %EXAMPS%\StimulusResponse.txs < %TEST%\StimulusResponse.txscmd
 TIMEOUT /T 10 /NOBREAK
-move /Y testTrace testTrace.Spec.StimulusResponse.Sut.StimulusResponse
+move /Y testTrace.log testTrace.Spec.StimulusResponse.Sut.StimulusResponse.log
 echo ------- End StimulusResponse Test 1
 
 echo ------- Start StimulusResponse Test 2
 start /min java -cp %EXAMPS% StimulusNoResponse
 call %TORXAKIS% %EXAMPS%\StimulusResponse.txs < %TEST%\StimulusResponse.txscmd
 TIMEOUT /T 10 /NOBREAK
-move /Y testTrace testTrace.Spec.StimulusResponse.Sut.StimulusNoResponse
+move /Y testTrace.log testTrace.Spec.StimulusResponse.Sut.StimulusNoResponse.log
 echo ------- End StimulusResponse Test 2
 
 echo ------- Start StimulusResponse Test 3
 start /min java -cp %EXAMPS% StimulusResponseLoop
 call %TORXAKIS% %EXAMPS%\StimulusResponseLoop.txs < %TEST%\StimulusResponse.txscmd
 TIMEOUT /T 10 /NOBREAK
-move /Y testTrace testTrace.Spec.StimulusResponseLoop.Sut.StimulusResponseLoop
+move /Y testTrace.log testTrace.Spec.StimulusResponseLoop.Sut.StimulusResponseLoop.log
 echo ------- End StimulusResponse Test 3
 
 echo ------- Start StimulusResponse Test 4
 start /min java -cp %EXAMPS% StimulusResponse
 call %TORXAKIS% %EXAMPS%\StimulusResponseLoop.txs < %TEST%\StimulusResponse.txscmd
 TIMEOUT /T 10 /NOBREAK
-move /Y testTrace testTrace.Spec.StimulusResponseLoop.Sut.StimulusResponse
+move /Y testTrace.log testTrace.Spec.StimulusResponseLoop.Sut.StimulusResponse.log
 echo ------- End StimulusResponse Test 4
 
 echo ------- Start StimulusResponse Test 5
 start /min java -cp %EXAMPS% StimulusResponseLoop
 call %TORXAKIS% %EXAMPS%\StimulusResponse.txs < %TEST%\StimulusResponse.txscmd
 TIMEOUT /T 10 /NOBREAK
-move /Y testTrace testTrace.Spec.StimulusResponse.Sut.StimulusResponseLoop
+move /Y testTrace.log testTrace.Spec.StimulusResponse.Sut.StimulusResponseLoop.log
 echo ------- End StimulusResponse Test 5
