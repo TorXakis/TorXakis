f83f75c56e581492b1c619e932f65e7544683648
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-07-05 10:12:26 +0200

    Fixed all dispatchprocess examples
    Change sleep time of dispatchprocess Sut to max 2s, same with TorXakis
     default timeout parameter
    Add parseAll.bat to parse all .txs files in repository
    Most examples didn't have CHANDEF; add them
    Remove duplicate DisPro3
    
    DisPro11-robust:
    - Added condition to ensure there is less difference between gcd params
    
    DisPro12-unique-id:
    - Added condition to ensure there is less difference between gcd params
    - Synchronized first two steps of dispatcher procedure to fix infinite
     possibilities for jobid
    - Fixed processor assumptions for traceRight model and purpose

---
+++ test/bin/parseAll.bat
@@ -0,0 +1,12 @@
+@echo off
+echo exit > parseAll.tmp
+for /f "usebackq" %%A in (`dir /S /B ^| findstr /E "\.txs"`) DO CALL :Body %%A
+del parseAll.tmp
+REM Don't "fall through" to :Body.
+GOTO :EOF
+
+:Body
+echo %1
+call torxakis.bat %1  < parseAll.tmp
+TIMEOUT /T 3 /NOBREAK > nul
+GOTO :EOF
