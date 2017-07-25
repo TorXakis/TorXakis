95634a4564ff8491e9464bed4b080be204db0063
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-07-25 17:00:52 +0200

    Added purpose tests for DisPro

--- test/examps/DispatchProcess/TestDispatchProcess.bat
+++ test/examps/DispatchProcess/TestDispatchProcess.bat
@@ -16,117 +16,133 @@ echo EXAMPSUTS %EXAMPSUTS%
 echo TEST %TEST%
 
 
-echo ------- Start DisPro01-processor Test
-call %TORXAKIS% %EXAMPS%\DisPro01-processor.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro01-processor.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro01-processor Test
-
-
-echo ------- Start DisPro02-dispatch Test
-call %TORXAKIS% %EXAMPS%\DisPro02-dispatch.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro02-dispatch.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro02-dispatch Test
-
-
-echo ------- Start DisPro03-processors Test
-call %TORXAKIS% %EXAMPS%\DisPro03-processors.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro03-processors.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro03-processors Test
-
-
-echo ------- Start DisPro04-hide Test
-call %TORXAKIS% %EXAMPS%\DisPro04-hide.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro04-hide.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro04-hide Test
-
-
-echo ------- Start DisPro05-data Test
-call %TORXAKIS% %EXAMPS%\DisPro05-data.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro05-data.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro05-data Test
-
-
-echo ------- Start DisPro05a-data-nohide Test
-call %TORXAKIS% %EXAMPS%\DisPro05a-data-nohide.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro05a-data-nohide.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro05a-data-nohide Test
-
-
-echo ------- Start DisPro06-datapos Test
-call %TORXAKIS% %EXAMPS%\DisPro06-datapos.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro06-datapos.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro06-datapos Test
-
-
-echo ------- Start DisPro06a-datapos-nohide Test
-call %TORXAKIS% %EXAMPS%\DisPro06a-datapos-nohide.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro06a-datapos-nohide.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro06a-datapos-nohide Test
-
-
-echo ------- Start DisPro07-gcd Test
-call %TORXAKIS% %EXAMPS%\DisPro07-gcd.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro07-gcd.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro07-gcd Test
-
-
-echo ------- Start DisPro08-gcdpurp Test
-call %TORXAKIS% %EXAMPS%\DisPro08-gcdpurp.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro08-gcdpurp.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro08-gcdpurp Test
-
-
-echo ------- Start DisPro09-multiproc Test
-call %TORXAKIS% %EXAMPS%\DisPro09-multiproc.txs < %TEST%\DispatchProcess_Stepper.txscmd
-TIMEOUT /T 2 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro09-multiproc.Sut.DispatchProcess_Stepper.log
-echo ------- End DisPro09-multiproc Test
-
-
-echo ------- Start DisPro10-data Test
-start /min java -cp %EXAMPSUTS% DispatchProcess
-call %TORXAKIS% %EXAMPS%\DisPro10-data.txs < %TEST%\DispatchProcess_Tester.txscmd
-TIMEOUT /T 4 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro10-data.Sut.DispatchProcess.log
-echo ------- End DisPro10-data Test
-
-
-REM echo ------- Start DisPro11-robust Test
-REM call %TORXAKIS% %EXAMPS%\DisPro11-robust.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM echo ------- Start DisPro01-processor Test
+REM call %TORXAKIS% %EXAMPS%\DisPro01-processor.txs < %TEST%\DispatchProcess_Stepper.txscmd
 REM TIMEOUT /T 2 /NOBREAK
-REM move /Y testTrace.log testTrace.Spec.DisPro11-robust.Sut.DispatchProcess_Stepper.log
-REM echo ------- End DisPro11-robust Test
+REM move /Y testTrace.log testTrace.Spec.DisPro01-processor.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro01-processor Test
 
 
-echo ------- Start DisPro12-unique-id Test
+REM echo ------- Start DisPro02-dispatch Test
+REM call %TORXAKIS% %EXAMPS%\DisPro02-dispatch.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro02-dispatch.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro02-dispatch Test
+
+
+REM echo ------- Start DisPro03-processors Test
+REM call %TORXAKIS% %EXAMPS%\DisPro03-processors.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro03-processors.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro03-processors Test
+
+
+REM echo ------- Start DisPro04-hide Test
+REM call %TORXAKIS% %EXAMPS%\DisPro04-hide.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro04-hide.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro04-hide Test
+
+
+REM echo ------- Start DisPro05-data Test
+REM call %TORXAKIS% %EXAMPS%\DisPro05-data.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro05-data.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro05-data Test
+
+
+REM echo ------- Start DisPro05a-data-nohide Test
+REM call %TORXAKIS% %EXAMPS%\DisPro05a-data-nohide.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro05a-data-nohide.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro05a-data-nohide Test
+
+
+REM echo ------- Start DisPro06-datapos Test
+REM call %TORXAKIS% %EXAMPS%\DisPro06-datapos.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro06-datapos.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro06-datapos Test
+
+
+REM echo ------- Start DisPro06a-datapos-nohide Test
+REM call %TORXAKIS% %EXAMPS%\DisPro06a-datapos-nohide.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro06a-datapos-nohide.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro06a-datapos-nohide Test
+
+
+REM echo ------- Start DisPro07-gcd Test
+REM call %TORXAKIS% %EXAMPS%\DisPro07-gcd.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro07-gcd.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro07-gcd Test
+
+
+REM echo ------- Start DisPro08-gcdpurp Test
+REM call %TORXAKIS% %EXAMPS%\DisPro08-gcdpurp.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro08-gcdpurp.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro08-gcdpurp Test
+
+
+REM echo ------- Start DisPro09-multiproc Test
+REM call %TORXAKIS% %EXAMPS%\DisPro09-multiproc.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM TIMEOUT /T 2 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro09-multiproc.Sut.DispatchProcess_Stepper.log
+REM echo ------- End DisPro09-multiproc Test
+
+
+REM echo ------- Start DisPro10-data Test
+REM start /min java -cp %EXAMPSUTS% DispatchProcess
+REM call %TORXAKIS% %EXAMPS%\DisPro10-data.txs < %TEST%\DispatchProcess_Tester.txscmd
+REM TIMEOUT /T 4 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro10-data.Sut.DispatchProcess.log
+REM echo ------- End DisPro10-data Test
+
+
+REM REM echo ------- Start DisPro11-robust Test
+REM REM call %TORXAKIS% %EXAMPS%\DisPro11-robust.txs < %TEST%\DispatchProcess_Stepper.txscmd
+REM REM TIMEOUT /T 2 /NOBREAK
+REM REM move /Y testTrace.log testTrace.Spec.DisPro11-robust.Sut.DispatchProcess_Stepper.log
+REM REM echo ------- End DisPro11-robust Test
+
+
+REM echo ------- Start DisPro12-unique-id Test
+REM start /min java -cp %EXAMPSUTS% DispatchProcess
+REM call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Tester_12.txscmd
+REM TIMEOUT /T 4 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcess.log
+REM echo ------- End DisPro12-unique-id Test
+
+
+REM echo ------- Start DisPro12-unique-id_Wrong Test
+REM start /min java -cp %EXAMPSUTS% DispatchProcess
+REM call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Tester_12_Wrong.txscmd
+REM TIMEOUT /T 4 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcess.log
+REM echo ------- End DisPro12-unique-id_Wrong Test
+
+
+
+
+REM echo ------- Start DisPro12-unique-id_Right Test
+REM start /min java -cp %EXAMPSUTS% DispatchProcess
+REM call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Tester_12_Right.txscmd
+REM TIMEOUT /T 4 /NOBREAK
+REM move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcessSut_12_Right.log
+REM echo ------- End DisPro12-unique-id_Right Test
+
+echo ------- Start DisPro12-unique-id_PurposeWrong Test
 start /min java -cp %EXAMPSUTS% DispatchProcess
-call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Tester_12.txscmd
+call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Tester_12_Wrong.txscmd
 TIMEOUT /T 4 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcess.log
-echo ------- End DisPro12-unique-id Test
+move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcessSut_12_PurposeWrong.log
+echo ------- End DisPro12-unique-id_PurposeWrong Test
 
-
-echo ------- Start DisPro12-unique-id_Wrong Test
+echo ------- Start DisPro12-unique-id_PurposeRight Test
 start /min java -cp %EXAMPSUTS% DispatchProcess
-call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Stepper_12_Wrong.txscmd
+call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Tester_12_Right.txscmd
 TIMEOUT /T 4 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcess.log
-echo ------- End DisPro12-unique-id_Wrong Test
-
-
-echo ------- Start DisPro12-unique-id_Right Test
-start /min java -cp %EXAMPSUTS% DispatchProcess
-call %TORXAKIS% %EXAMPS%\DisPro12-unique-id.txs < %TEST%\DispatchProcess_Stepper_12_Right.txscmd
-TIMEOUT /T 4 /NOBREAK
-move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcess.log
-echo ------- End DisPro12-unique-id_Right Test
+move /Y testTrace.log testTrace.Spec.DisPro12-unique-id.Sut.DispatchProcessSut_12_PurposeRight.log
+echo ------- End DisPro12-unique-id_PurposeRight Test
