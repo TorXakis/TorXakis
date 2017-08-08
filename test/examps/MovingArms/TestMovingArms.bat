9d1b362019547e89711a2ce2d73c3a1fd56b5c28
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-08-08 16:44:39 +0200

    Renamed MovingArms, Point example test files

--- test/examps/MovingArms/TestMovingArms.bat
+++ test/examps/MovingArms/TestMovingArms.bat
@@ -16,27 +16,27 @@ echo TEST %TEST%
 
 
 echo ------- Start MovingArms Stepper Test
-call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Step.txscmd
+call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Stepper.txscmd
 TIMEOUT /T 4 /NOBREAK
-move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Step.log
+move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Stepper.log
 echo ------- End MovingArms Stepper Test
 
 REM echo ------- Start MovingArms Simulator Test
-REM start "" /min /b call %TORXAKISPORT% 9877 %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Sim.txscmd
-REM call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Test.txscmd
+REM start "" /min /b call %TORXAKISPORT% 9877 %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Simulator.txscmd
+REM call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Tester.txscmd
 REM TIMEOUT /T 4 /NOBREAK
 REM move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Sim.log
 REM echo ------- End MovingArms Simulator Test
 
 echo ------- Start MovingArms_Purpose Stepper Test
-call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Purpose_Step.txscmd
+call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Purpose_Stepper.txscmd
 TIMEOUT /T 4 /NOBREAK
-move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Purpose_Step.log
+move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Purpose_Stepper.log
 echo ------- End MovingArms_Purpose Stepper Test
 
 REM echo ------- Start MovingArms Simulator Test
-REM start "" /min /b call %TORXAKISPORT% 9877 %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Purpose_Sim.txscmd
-REM call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Purpose_Test.txscmd
+REM start "" /min /b call %TORXAKISPORT% 9877 %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Purpose_Simulator.txscmd
+REM call %TORXAKIS% %EXAMPS%\MovingArms.txs < %TEST%\MovingArms_Purpose_Tester.txscmd
 REM TIMEOUT /T 4 /NOBREAK
-REM move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Purpose_Sim.log
+REM move /Y testTrace.log testTrace.Spec.MovingArms.Sut.MovingArms_Purpose_Simulator.log
 REM echo ------- End MovingArms Simulator Test
