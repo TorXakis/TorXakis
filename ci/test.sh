044b26fa3bf37c5717a03b0cdd0f9d6134f25243
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-09-13 15:01:19 +0200

    Run long tests in parallel
    Marks CustomersOrders and MovingArms-PurposeStepper Tests #long
    Splits buildAndTest.sh into build.sh, test.sh and test_long.sh in
     order to run long tests in parallel with the rest

---
+++ ci/test.sh
@@ -0,0 +1,1 @@
+cd test/sqatt && stack test --test-arguments="--skip=#long" --work-dir $CACHE_DIR_REL/test/.stack_work --stack-root $CACHE_DIR/.stack --allow-different-user && cd -
---
+++ ci/test_long.sh
@@ -0,0 +1,1 @@
+cd test/sqatt && stack test --test-arguments="--match=#long" --work-dir $CACHE_DIR_REL/test/.stack_work --stack-root $CACHE_DIR/.stack --allow-different-user && cd -
