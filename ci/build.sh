63db12b3dd03edce291f18067009ed0d81875f11
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-09-15 14:21:14 +0200

    Disabled unnecessary export of path in build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,2 +1,2 @@
-export PATH=$HOME/.local/bin:$CACHE_DIR/z3/bin:$CACHE_DIR/bin:$PATH
+# export PATH=$HOME/.local/bin:$CACHE_DIR/z3/bin:$CACHE_DIR/bin:$PATH
 stack install --install-ghc --haddock --test --fast --stack-yaml stack_linux.yaml --work-dir $CACHE_DIR_REL/.stack-work --stack-root $CACHE_DIR/.stack --allow-different-user
