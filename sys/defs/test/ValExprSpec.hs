1e39ee6892c74d8d9201bdefdbd52197ddf8bff7
capitanbatata <damian.nadales@gmail.com>
2017-09-21 16:24:35 +0200

    Introduced the BopPolinomial. [skip ci]

--- sys/defs/test/ValExprSpec.hs
+++ sys/defs/test/ValExprSpec.hs
@@ -1,3 +1,9 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2016 TNO and Radboud University
+See license.txt
+-}
+{-# OPTIONS -Wall -Werror #-}
 module ValExprSpec where
 
 import           Sum
@@ -13,21 +19,9 @@ sum12 :: ValExpr Int
 sum12 = cstrSum $ fromMultiplierList  [ (cstrConst (Cint 1), 1)
                                       , (cstrConst (Cint 2), 1)]
 
-sum01True :: ValExpr Int
-sum01True = cstrSum $ fromMultiplierList  [ (cstrConst (Cint 0), 1)
-                                       , (cstrConst (Cint 1), 1)
-                                       , (cstrConst (Cbool True), 1)]
-
-sum1True :: ValExpr Int
-sum1True = cstrSum $ fromMultiplierList  [ (cstrConst (Cint 1), 1)
-                                         , (cstrConst (Cbool True), 1)]
 spec :: Spec
 spec =
-  describe "cstrSum examples" $ do
+  describe "cstrSum examples" $
     it "Transforms (0 + 1) + 2 into (0 + 1 + 2)" $
       cstrSum (fromMultiplierList [(cstrConst (Cint 0), 1), (sum12, 1)])
       `shouldBe` sum012
-    it "Does not allow to sum Integers and Booleans" $
-      -- TODO: this shouldn't even compile!
-      cstrSum (fromMultiplierList [(cstrConst (Cint 0), 1), (sum1True, 1)])
-      `shouldBe` sum01True
