ff0d09471a06aacbc0c2b918f088a5baff47a7a8
capitanbatata <damian.nadales@gmail.com>
2017-10-02 17:11:25 +0200

    Migrated choice.

--- test/sqatt/src/Benchmarks/All.hs
+++ test/sqatt/src/Benchmarks/All.hs
@@ -1,8 +1,11 @@
 module Benchmarks.All (allBenchmarks) where
 
+import qualified Benchmarks.Choice   as Choice
 import qualified Benchmarks.Sequence as Sequence
 import           Criterion.Main
 import           Sqatt
 
 allBenchmarks :: [Benchmark]
-allBenchmarks = benchmarkExampleSet <$> [ Sequence.benchmarksSet ]
+allBenchmarks = benchmarkExampleSet <$> [ Sequence.benchmarksSet
+                                        , Choice.benchmarksSet
+                                        ]
---
+++ test/sqatt/src/Benchmarks/Choice.hs
@@ -0,0 +1,26 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+{-# LANGUAGE OverloadedStrings #-}
+module Benchmarks.Choice (benchmarksSet) where
+
+import           Examples.Paths
+import           Prelude        hiding (FilePath)
+import           Sqatt
+
+benchDir :: FilePath
+benchDir = "Choice"
+
+do100Choices :: TxsExample
+do100Choices = TxsExample
+    { exampleName = "100 choices"
+    , txsModelFiles = [txsFilePathBench benchDir "Choice"]
+    , txsCommandsFile = txsCmdPathBench benchDir "ForeverChoice"
+    , sutExample = Nothing
+    , expectedResult = Pass
+    }
+
+benchmarksSet :: TxsExampleSet
+benchmarksSet = TxsExampleSet "Choice" [ do100Choices ]
