42dbc1870368cd2dae04d29258b88859a4901ae4
capitanbatata <damian.nadales@gmail.com>
2017-10-02 18:52:58 +0200

    Added benchmarks for synchronization.

--- test/sqatt/src/Benchmarks/All.hs
+++ test/sqatt/src/Benchmarks/All.hs
@@ -1,11 +1,13 @@
 module Benchmarks.All (allBenchmarks) where
 
-import qualified Benchmarks.Choice   as Choice
-import qualified Benchmarks.Sequence as Sequence
+import qualified Benchmarks.Choice          as Choice
+import qualified Benchmarks.Sequence        as Sequence
+import qualified Benchmarks.Synchronization as Synchronization
 import           Criterion.Main
 import           Sqatt
 
 allBenchmarks :: [Benchmark]
-allBenchmarks = benchmarkExampleSet <$> [ Sequence.benchmarksSet
-                                        , Choice.benchmarksSet
+allBenchmarks = benchmarkExampleSet <$> [ Choice.benchmarksSet
+                                        , Sequence.benchmarksSet
+                                        , Synchronization.benchmarksSet
                                         ]
---
+++ test/sqatt/src/Benchmarks/Synchronization.hs
@@ -0,0 +1,56 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+{-# LANGUAGE OverloadedStrings #-}
+module Benchmarks.Synchronization (benchmarksSet) where
+
+import           Examples.Paths
+import           Prelude        hiding (FilePath)
+import           Sqatt
+
+benchDir :: FilePath
+benchDir = "Synchronization"
+
+modelFiles :: [FilePath]
+modelFiles = [ txsFilePathBench benchDir "Synchronization"
+             , txsFilePathBench "Sequence" "SingleActionSequence"
+             ]
+
+-- do100Acts3procs :: TxsExample
+-- do100Acts3procs = TxsExample
+--     { exampleName = "3 processes"
+--     , txsModelFiles = modelFiles
+--     , txsCommandsFile = txsCmdPathBench benchDir "ForeverSynchronized3"
+--     , sutExample = Nothing
+--     , expectedResult = Pass
+--     }
+
+-- do100Acts6procs :: TxsExample
+-- do100Acts6procs = TxsExample
+--     { exampleName = "6 processes"
+--     , txsModelFiles = modelFiles
+--     , txsCommandsFile = txsCmdPathBench benchDir "ForeverSynchronized6"
+--     , sutExample = Nothing
+--     , expectedResult = Pass
+--     }
+
+alternateTwoProcs :: TxsExample
+alternateTwoProcs = TxsExample
+    { exampleName = "alternate 2 processes"
+    , txsModelFiles = modelFiles
+    , txsCommandsFile = txsCmdPathBench benchDir "ForeverSyncAlt2"
+    , sutExample = Nothing
+    , expectedResult = Pass
+    }
+
+benchmarksSet :: TxsExampleSet
+-- TODO: do100Acts3procs hangs TorXakis, see https://github.com/TorXakis/TorXakis/issues/268
+-- benchmarksSet = TxsExampleSet "Synchronization" [ do100Acts3procs
+--                                                 , do100Acts6procs
+--                                                 , alternateTwoProcs
+--                                                 ]
+benchmarksSet = TxsExampleSet "Synchronization" [ alternateTwoProcs ]
+
+
