52521f60d0afd395111bcb181f9e3727c0f614ff
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-10-19 16:32:39 +0200

    Added ExploreModels test-suite to sqatt
    Only with ControlLoop tests, which don't work yet

---
+++ test/sqatt/src/ExploreModels/All.hs
@@ -0,0 +1,13 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+{-# LANGUAGE OverloadedStrings #-}
+module ExploreModels.All (allTests) where
+
+import qualified ExploreModels.ControlLoop as ControlLoop
+import           Sqatt
+
+allTests :: [TxsExampleSet]
+allTests = [ ControlLoop.exampleSet ]
