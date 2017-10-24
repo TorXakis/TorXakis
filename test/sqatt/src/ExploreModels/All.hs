10d0b7b635946b3e58445e5ae787d070a79fb7aa
Kerem Ispirli <kerem.ispirli@tno.nl>
2017-10-24 10:23:49 +0200

    Added model test for LuckyPeople

--- test/sqatt/src/ExploreModels/All.hs
+++ test/sqatt/src/ExploreModels/All.hs
@@ -9,10 +9,12 @@ module ExploreModels.All (allTests) where
 import qualified ExploreModels.ControlLoop as ControlLoop
 import qualified ExploreModels.CustomersOrders as CustomersOrders
 import qualified ExploreModels.DispatchProcess as DispatchProcess
+import qualified ExploreModels.LuckyPeople as LuckyPeople
 import           Sqatt
 
 allTests :: [TxsExampleSet]
 allTests = [ ControlLoop.exampleSet
            , CustomersOrders.exampleSet
            , DispatchProcess.exampleSet
+           , LuckyPeople.exampleSet
            ]
