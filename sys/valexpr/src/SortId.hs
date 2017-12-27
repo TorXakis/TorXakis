83502265952f1916ae5131cc9739783e07166bbf
pjljvandelaar <pierre.vandelaar@tno.nl>
2017-12-27 16:37:32 +0100

    Improved haddock documentation (#557)

--- sys/valexpr/src/SortId.hs
+++ sys/valexpr/src/SortId.hs
@@ -3,8 +3,18 @@ TorXakis - Model Based Testing
 Copyright (c) 2015-2017 TNO and Radboud University
 See LICENSE at root directory of this repository.
 -}
-
--- ----------------------------------------------------------------------------------------- --
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  SortId
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+-- 
+-- Maintainer  :  pierre.vandelaar@tno.nl (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Data structure for identifiers for sorts
+-----------------------------------------------------------------------------
 {-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric      #-}
@@ -20,28 +30,35 @@ import           Id
 import           Name
 
 
+-- | Identifier for Sort
 data SortId = SortId
     { name :: Name            -- capid
     , unid :: Id
     } deriving (Eq, Ord, Read, Show, Generic, NFData, Data)
 
+-- | SortId is Resettable
 instance Resettable SortId
+
+-- | SortId is Identifiable
 instance Identifiable SortId
 
 -- * standard sorts
-
+-- | Identifier for Sort Bool
 sortIdBool :: SortId
 sortIdBool = SortId "Bool" 101
 
+-- | Identifier for Sort Int
 sortIdInt :: SortId
 sortIdInt = SortId "Int" 102
 
+-- | Identifier for Sort String
 sortIdString :: SortId
 sortIdString = SortId "String" 104
 
+-- | Identifier for Sort Regex
 sortIdRegex :: SortId
 sortIdRegex = SortId "Regex" 105
 
+-- | Identifier for Sort Error
 sortIdError :: SortId
 sortIdError = SortId "_Error" (-1)
-
--- sys/valexpr/src/ValExpr.hs
+++ sys/valexpr/src/ValExpr.hs
@@ -3,10 +3,18 @@ TorXakis - Model Based Testing
 Copyright (c) 2015-2017 TNO and Radboud University
 See LICENSE at root directory of this repository.
 -}
--- ----------------------------------------------------------------------------------------- --
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  ValExpr
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+-- 
+-- Maintainer  :  pierre.vandelaar@tno.nl (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
 --
--- Value Expressions
--- ----------------------------------------------------------------------------------------- --
+-- Interface file for Value Expressions.
+-----------------------------------------------------------------------------
 module ValExpr
 ( ValExpr
 , view
