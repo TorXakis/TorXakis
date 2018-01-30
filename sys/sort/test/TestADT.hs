4090524a86bd98ab997fd3c09923edc4f30cd35d
Kerem Ispirli <kerem.ispirli@tno.nl>
2018-01-30 11:38:09 +0100

    Split ADT test cases

--- sys/sort/test/TestADT.hs
+++ sys/sort/test/TestADT.hs
@@ -23,7 +23,8 @@ import SortInternal
 -- ----------------------------------------------------------------------------
 testADTList :: Test
 testADTList  = TestList [ TestLabel "Ref Test" testRef
-                        , TestLabel "ADT analysis Test" testADTAnalysis
+                        , TestLabel "Test Constructable ADTs" testADTAnalysisConstructable
+                        , TestLabel "Test Non-Constructable ADTs" testADTAnalysisNonConstructable
                         ]
 
 ---------------------------------------------------------------------------
@@ -36,56 +37,15 @@ testRef = TestCase $ do
         rInt = Ref expected
     assertEqual "Same reference?" expected $ toInt rInt
 
-testADTAnalysis :: Test
-testADTAnalysis = TestCase $ do
+testADTAnalysisConstructable :: Test
+testADTAnalysisConstructable = TestCase $ do
     let adtList = [(adtARef, adtA),(adtBRef, adtB),(adtCRef, adtC)]
         cADTs   = Map.fromList adtList
-        -- A { b :: B }
-        adtARef = Ref 1
-        adtA = ADTDef { adtName = "A"
-                      , constructors = cDefsA
-                      }
-        Right cDefsA = constructorDefs [(Ref 1, cstrA)]
-        cstrA = ConstructorDef { constructorName = "cstrA"
-                               , fields = fDefsA
-                               }
-        Right fDefsA = fieldDefs [(Ref 1, fieldB)]
-        fieldB = FieldDef { fieldName = "fieldB"
-                          , sort = SortADT adtBRef}
-        -- B { a :: A } | B { c :: C }
-        adtBRef = Ref 2
-        adtB = ADTDef { adtName = "B"
-        , constructors = cDefsB
-        }
-        Right cDefsB = constructorDefs [(Ref 1, cstrB1), (Ref 2, cstrB2)]
-        -- B { a :: A }
-        cstrB1 = ConstructorDef { constructorName = "cstrB1"
-                        , fields = fDefsB1
-                        }
-        Right fDefsB1 = fieldDefs [(Ref 1, fieldA)]
-        fieldA = FieldDef { fieldName = "fieldA"
-                    , sort = SortADT adtARef}
-        -- B { c :: C }
-        cstrB2 = ConstructorDef { constructorName = "cstrB2"
-                        , fields = fDefsB2
-                        }
-        Right fDefsB2 = fieldDefs [(Ref 1, fieldC)]
-        fieldC = FieldDef { fieldName = "fieldC"
-                    , sort = SortADT adtCRef}
-        -- C { i :: Int }
-        adtCRef = Ref 3
-        adtC = ADTDef { adtName = "C"
-                      , constructors = cDefsC
-                      }
-        Right cDefsC = constructorDefs [(Ref 1, cstrC)]
-        cstrC = ConstructorDef { constructorName = "cstrC"
-                               , fields = fDefsC
-                               }
-        Right fDefsC = fieldDefs [(Ref 1, fieldInt)]
-        fieldInt = FieldDef { fieldName = "fieldInt"
-                          , sort = SortInt}
     assertEqual "All data types should be constructable" (cADTs,[])
         $ analyzeADTs (adtDefsToMap emptyADTDefs,[]) adtList
+
+testADTAnalysisNonConstructable :: Test
+testADTAnalysisNonConstructable = TestCase $ do
     let -- B { a :: A }
         adtBRef' = Ref 4
         adtB' = ADTDef { adtName = "B"
@@ -98,4 +58,49 @@ testADTAnalysis = TestCase $ do
     assertEqual "Only C should be constructable" (cADTs',reverse ncADTs)
         $ analyzeADTs (adtDefsToMap emptyADTDefs,[]) adtList'
 
-        -- todo: split these cases
+-- Test Data
+
+-- A { b :: B }
+adtARef = Ref 1
+adtA = ADTDef { adtName = "A"
+              , constructors = cDefsA
+              }
+Right cDefsA = constructorDefs [(Ref 1, cstrA)]
+cstrA = ConstructorDef { constructorName = "cstrA"
+                       , fields = fDefsA
+                       }
+Right fDefsA = fieldDefs [(Ref 1, fieldB)]
+fieldB = FieldDef { fieldName = "fieldB"
+                  , sort = SortADT adtBRef}
+-- B { a :: A } | B { c :: C }
+adtBRef = Ref 2
+adtB = ADTDef { adtName = "B"
+, constructors = cDefsB
+}
+Right cDefsB = constructorDefs [(Ref 1, cstrB1), (Ref 2, cstrB2)]
+-- B { a :: A }
+cstrB1 = ConstructorDef { constructorName = "cstrB1"
+                , fields = fDefsB1
+                }
+Right fDefsB1 = fieldDefs [(Ref 1, fieldA)]
+fieldA = FieldDef { fieldName = "fieldA"
+            , sort = SortADT adtARef}
+-- B { c :: C }
+cstrB2 = ConstructorDef { constructorName = "cstrB2"
+                , fields = fDefsB2
+                }
+Right fDefsB2 = fieldDefs [(Ref 1, fieldC)]
+fieldC = FieldDef { fieldName = "fieldC"
+            , sort = SortADT adtCRef}
+-- C { i :: Int }
+adtCRef = Ref 3
+adtC = ADTDef { adtName = "C"
+              , constructors = cDefsC
+              }
+Right cDefsC = constructorDefs [(Ref 1, cstrC)]
+cstrC = ConstructorDef { constructorName = "cstrC"
+                       , fields = fDefsC
+                       }
+Right fDefsC = fieldDefs [(Ref 1, fieldInt)]
+fieldInt = FieldDef { fieldName = "fieldInt"
+                  , sort = SortInt}
