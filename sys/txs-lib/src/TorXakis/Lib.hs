24c91e132c7d831cb44519bea4fa1864909fdddf
capitanbatata <damian.nadales@gmail.com>
2018-02-26 18:44:49 +0100

    The first version of the `load` function that parses the string passed as parameter.

--- sys/txs-lib/src/TorXakis/Lib.hs
+++ sys/txs-lib/src/TorXakis/Lib.hs
@@ -3,24 +3,58 @@
 module TorXakis.Lib where
 
 import           Data.Text (Text)
+import           Control.Concurrent.STM.TVar (TVar, newTVarIO, modifyTVar)
+import           Control.Monad.STM (atomically)
+-- See TODO below.
+-- import           Path
+
+import           Sigs     (Sigs, empty)
+import           VarId    (VarId)
+import           TxsDefs  (TxsDefs, empty)
+import           TxsAlex  (txsLexer)
+import           TxsHappy (txsParser)
 
 data Response = Success | Error { msg :: Text }
 
-data FileName
-
-data FileContents
+-- | For now file contents are represented as a string. This has to change in
+-- the future, since it is quite inefficient, but we start off simple since the
+-- current 'TorXakis' parser parses @String@s.
+type FileContents = String
 
 data ModelName
 
--- | The session monad, which maintains the state of a TorXakis model.
-data Session a
+-- | The session, which maintains the state of a TorXakis model.
+newtype Session = Session
+    { _sessionState :: TVar SessionSt }
+
+data SessionSt = SessionSt
+    { _tdefs   :: TxsDefs
+    , _sigs    :: Sigs VarId
+    }
+
+emptySessionState :: SessionSt
+emptySessionState = SessionSt TxsDefs.empty Sigs.empty 
+
+-- | Create a new session.
+newSession :: IO Session    
+newSession = Session <$> newTVarIO emptySessionState
 
 -- | Load a TorXakis file, compile it, and return the response.
-load :: FileName -> FileContents -> Session Response
-load = undefined
+--
+-- The absolute path to the file is used to associate the parsed structure
+-- (using the file-contents) with the file name, so that by calling `unload` we
+-- know what to unload.
+--
+-- In the future we might want to make loading of 'TorXakis' models
+-- incremental, to give better error messages, and support more modularity.
+load :: Session -> FileContents -> IO Response 
+load s xs = do
+    let (_, ts, is) = txsParser . txsLexer $ xs 
+    atomically $ modifyTVar (_sessionState s) (const (SessionSt ts is))
+    return Success
 
 -- | Start the stepper with the current model.
-stepper :: ModelName -> Session Response
+stepper :: Session -> ModelName -> IO Response
 stepper = undefined
 
 -- | How a step is described
@@ -38,5 +72,5 @@ data ActionName
 data Steps
 
 -- | step for n-steps
-step :: StepType -> Session Response -- | Or a conduit?
+step :: Session -> StepType -> IO Response -- | Or a conduit?
 step = undefined
