46fcf0307f390e3a728cafb7a82be6f189908979
capitanbatata <damian.nadales@gmail.com>
2018-03-12 15:32:05 +0100

    Add a `txs-compiler` package, with the API definition of the compiler and parser.

---
+++ sys/txs-compiler/src/TorXakis/Compiler.hs
@@ -0,0 +1,25 @@
+module TorXakis.Compiler where
+
+import           Data.Text (Text)
+import           Control.Arrow ((|||))
+    
+import           Id      (Id)
+import           TxsDefs (TxsDefs)
+import           Sigs    (Sigs)
+import           VarId   (VarId)
+
+import           TorXakis.Compiler.Error (Error)
+
+-- | Compile a string into a TorXakis model.
+--
+-- TODO: for now we use String's to be able to leverage on the 'haskell' token
+-- parser, in the future we might want to change this to text, and benchmark
+-- what is the performance gain.
+compile :: String -> Either Error (Id, TxsDefs, Sigs VarId)
+compile = undefined
+
+-- | Legacy compile function, used to comply with the old interface. It should
+-- be deprecated in favor of @compile@.
+compileLegacy :: String -> (Id, TxsDefs, Sigs VarId)
+compileLegacy = (throwOnLeft ||| id) . compile 
+    where throwOnLeft = error . show
---
+++ sys/txs-compiler/src/TorXakis/Compiler/Error.hs
@@ -0,0 +1,8 @@
+-- | 
+
+module TorXakis.Compiler.Error where
+
+import           Data.Text (Text)
+
+-- | For now we use this simple error type.
+type Error = Text
