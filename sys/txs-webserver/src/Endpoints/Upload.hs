adad8caba73ec038a185f6da7107970ea2190f55
Kerem Ispirli <kerem.ispirli@tno.nl>
2018-03-13 11:25:21 +0100

    Implemented loading a file into TorXakis session[skip ci]

--- sys/txs-webserver/src/Endpoints/Upload.hs
+++ sys/txs-webserver/src/Endpoints/Upload.hs
@@ -3,26 +3,47 @@ module Endpoints.Upload
 , upload
 ) where
 
+import           Control.Concurrent.STM.TVar (readTVarIO)
 import           Control.Monad.IO.Class      (liftIO)
-import qualified Data.ByteString.Lazy        as LBS
-import           Data.Foldable
+import           Control.Monad.Trans.Reader  (ask)
+import qualified Data.IntMap.Strict          as Map
+import           Data.Maybe                  (fromMaybe)
+import           Data.Text.Lazy              (unpack)
+import           Data.Text.Lazy.Encoding     (decodeUtf8)
 import           Servant
-import           Servant.Multipart           (MultipartForm, MultipartData, Mem, iName, iValue, inputs, files, fdPayload, fdFileName)
+import           Servant.Multipart           (MultipartForm, MultipartData, Mem, FileData, files, fdFileName, fdPayload) --  iName, iValue, inputs
 
-import           Common (SessionId, TxsHandler)
+import           TorXakis.Lib                (load, Response (..))
+import           TorXakis.Session            (Session)
 
-type UploadEP = "session" :> Capture "sid" SessionId :> "model" :> MultipartForm Mem (MultipartData Mem) :> PostCreated '[JSON] Integer
+import           Common (SessionId, TxsHandler, Env (..))
 
-upload :: SessionId -> MultipartData Mem -> TxsHandler Integer
-upload _sid multipartData =  do
+type UploadEP = "session" :> Capture "sid" SessionId :> "model" :> MultipartForm Mem (MultipartData Mem) :> PostCreated '[JSON] String
+
+upload :: SessionId -> MultipartData Mem -> TxsHandler String
+upload sid multipartData =  do
+    Env{sessions = ssT} <- ask
+    sessionsMap <- liftIO $ readTVarIO ssT
+    let s = fromMaybe
+                (error $ "Session " ++ show sid ++ " not found.")
+                $ Map.lookup sid sessionsMap
+
     liftIO $ do
-        putStrLn "Inputs:"
-        forM_ (inputs multipartData) $ \input ->
-            putStrLn $ "  " ++ show (iName input)
-                    ++ " -> " ++ show (iValue input)
-    
-        forM_ (files multipartData) $ \file -> do
-            let content = fdPayload file
-            putStrLn $ "Content of " ++ show (fdFileName file)
-            LBS.putStr content
-    return 0
+        rs <- mapM (loadFile s) $ files multipartData
+        let errMsg = concatErrorMsgs rs
+        case errMsg of
+            []   -> return $ show Success
+            eMsg -> error eMsg
+
+loadFile :: Session -> FileData Mem -> IO Response
+loadFile s f = do
+    putStrLn $ "Loading file: " ++ show (fdFileName f)
+    let contentTxt = decodeUtf8 $ fdPayload f
+    r <- load s $ unpack contentTxt
+    print r
+    return r
+
+concatErrorMsgs :: [Response] -> String
+concatErrorMsgs []               = []
+concatErrorMsgs (Error m : rs) = m ++ "\n" ++ concatErrorMsgs rs
+concatErrorMsgs (Success : rs)   = concatErrorMsgs rs
