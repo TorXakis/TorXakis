fea52805826eeb87794b7f896e62826813c8cb34
Kerem Ispirli <kerem.ispirli@tno.nl>
2018-04-24 14:46:19 +0200

    Fixed response of session creation

--- sys/txs-webserver/src/Endpoints/NewSession.hs
+++ sys/txs-webserver/src/Endpoints/NewSession.hs
@@ -3,6 +3,7 @@ TorXakis - Model Based Testing
 Copyright (c) 2015-2017 TNO and Radboud University
 See LICENSE at root directory of this repository.
 -}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Endpoints.NewSession
     ( NewSessionEP
@@ -12,20 +13,26 @@ module Endpoints.NewSession
 import           Control.Concurrent.STM.TVar (modifyTVar, readTVarIO, writeTVar)
 import           Control.Monad.IO.Class      (liftIO)
 import           Control.Monad.STM           (atomically)
+import           Data.Aeson                  (ToJSON)
 import qualified Data.IntMap.Strict          as Map
+import           GHC.Generics                (Generic)
 import           Servant
 
-import           TorXakis.Lib     (newSession)
+import           TorXakis.Lib                (newSession)
 
-import           Common (SessionId, Env(..))
+import           Common                      (Env (..))
 
-type NewSessionEP = "sessions" :> "new" :> PostCreated '[JSON] SessionId
+newtype SessionIdResponse = SessionIdResponse{sessionId :: Int}
+    deriving (Generic)
+instance ToJSON SessionIdResponse
 
-newSrvSession :: Env -> Handler SessionId
+type NewSessionEP = "sessions" :> "new" :> PostCreated '[JSON] SessionIdResponse
+
+newSrvSession :: Env -> Handler SessionIdResponse
 newSrvSession Env{sessions = ssT, lastSid = pSidT} = do
     pSid       <- liftIO $ readTVarIO pSidT
     s <- liftIO newSession
     let sid = pSid + 1
     liftIO $ atomically $ modifyTVar ssT   (Map.insert sid s)
     liftIO $ atomically $ writeTVar  pSidT sid
-    return sid
+    return SessionIdResponse{sessionId = sid}
