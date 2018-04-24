437c6f5445ecabff704a96218217a212ddbb5f61
Kerem Ispirli <kerem.ispirli@tno.nl>
2018-04-24 14:46:02 +0200

    Updated Spec to use proper API endpoints; many tests fail

--- sys/txs-webserver/test/Spec.hs
+++ sys/txs-webserver/test/Spec.hs
@@ -32,24 +32,24 @@ spec = return $ do
         describe "Get TorXakis info" $
             it "Info responds with 200 and info" $ do
                 r <- get "http://localhost:8080/info"
-                r ^. responseStatus . statusCode `shouldBe` 200
+                r ^. responseStatus . statusCode `shouldBe` 200 -- OK
                 let bodyStr = BSL.unpack $ r ^. responseBody
                 bodyStr `shouldContain` "\"version\""
                 bodyStr `shouldContain` "\"buildTime\""
         describe "Create new TorXakis session" $
             it "Creates 2 sessions" $ do
                 r <- post "http://localhost:8080/sessions/new" [partText "" ""]
-                r ^. responseStatus . statusCode `shouldBe` 201
+                r ^. responseStatus . statusCode `shouldBe` 201 -- Created
                 r ^. responseBody `shouldBe` "{\"sessionId\":1}"
                 r2 <- post "http://localhost:8080/sessions/new" [partText "" ""]
-                r2 ^. responseStatus . statusCode `shouldBe` 201
+                r2 ^. responseStatus . statusCode `shouldBe` 201 -- Created
                 r2 ^. responseBody `shouldBe` "{\"sessionId\":2}"
         describe "Upload files to a session" $ do
             it "Uploads valid files" $ do
                 _ <- post "http://localhost:8080/sessions/new" [partText "" ""]
-                r <- post "http://localhost:8080/sessions/1/load" [partFile "Point.txs" "../../examps/Point/Point.txs"]
-                r ^. responseStatus . statusCode `shouldBe` 202
-                r ^. responseBody `shouldBe` "{\"fileName\": \"Point.txs\"}"
+                r <- put "http://localhost:8080/sessions/1/model" [partFile "Point.txs" "../../examps/Point/Point.txs"]
+                r ^. responseStatus . statusCode `shouldBe` 202 -- Accepted
+                r ^. responseBody `shouldBe` "{\"loadedFile\": \"Point.txs\"}"
             it "Fails for parse error" $ do
                 let handler (CI.HttpExceptionRequest _ (C.StatusCodeException r body)) = do
                         BS.unpack body `shouldStartWith` "{\"msg\": \"Error in wrong.txt: \nParse Error:"
@@ -57,25 +57,24 @@ spec = return $ do
                         return CI.Response{CI.responseStatus = s}
                     handler e = throwIO e
                 _ <- post "http://localhost:8080/sessions/new" [partText "" ""]
-                r <- post "http://localhost:8080/sessions/1/load" [partFile "wrong.txt" "../../sys/txs-lib/test/data/wrong.txt"]
+                r <- put "http://localhost:8080/sessions/1/model" [partFile "wrong.txt" "../../sys/txs-lib/test/data/wrong.txt"]
                         `catch` handler
-                r ^. responseStatus . statusCode `shouldBe` 400
+                r ^. responseStatus . statusCode `shouldBe` 400 -- Bad Request
             it "Starts stepper and takes 3 steps" $ do
                 _ <- post "http://localhost:8080/sessions/new" [partText "" ""]
-                _ <- put "http://localhost:8080/sessions/1/load" [partFile "Point.txs" "../../examps/Point/Point.txs"]
-                post "http://localhost:8080/sessions/1/models/Model/stepper" [partText "" ""] >>= checkSuccess
-                post "http://localhost:8080/sessions/1/stepper/3" [partText "" ""] >>= checkSuccess
+                _ <- put "http://localhost:8080/sessions/1/model" [partFile "Point.txs" "../../examps/Point/Point.txs"]
+                post "http://localhost:8080/sessions/1/models/Model/stepper" [partText "" ""] >>= check204NoContent
+                post "http://localhost:8080/sessions/1/stepper/3" [partText "" ""] >>= check204NoContent
                 get "http://localhost:8080/sessions/1/messages" >>= checkJSON
             -- it "Starts tester and tests 3 steps" $ do
             --     _ <- post "http://localhost:8080/sessions/new" [partText "" ""]
-            --     _ <- post "http://localhost:8080/sessions/1/model" [partFile "Point.txs" "../../examps/Point/Point.txs"]
+            --     _ <- put "http://localhost:8080/sessions/1/model" [partFile "Point.txs" "../../examps/Point/Point.txs"]
             --     _ <- checkSuccess <$> post "http://localhost:8080/tester/start/1/Model" [partText "" ""]
             --     _ <- checkSuccess <$> post "http://localhost:8080/tester/test/1/3" [partText "" ""]
             --     checkJSON    <$> get "http://localhost:8080/sessions/sse/1/messages"
 
-checkSuccess :: Response BSL.ByteString -> IO ()
-checkSuccess r =
-    r ^. responseStatus . statusCode `shouldBe` 204
+check204NoContent :: Response BSL.ByteString -> IO ()
+check204NoContent r = r ^. responseStatus . statusCode `shouldBe` 204
 
 checkJSON :: Response BSL.ByteString -> IO ()
 checkJSON r = do
