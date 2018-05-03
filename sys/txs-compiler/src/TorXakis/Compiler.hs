9bf93d3a4340411b87e73bbc89eec2e01b2e97ad
Damian Nadales <damian.nadales@gmail.com>
2018-05-03 11:38:23 +0200

    Add a less complex structure to the `compileParsedDefs` functions by sorting out the data dependencies and splitting this function into smaller functions.

--- sys/txs-compiler/src/TorXakis/Compiler.hs
+++ sys/txs-compiler/src/TorXakis/Compiler.hs
@@ -1,64 +1,65 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TupleSections     #-}
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
 module TorXakis.Compiler where
 
-import           Control.Arrow                     (first, second, (|||))
-import           Control.Lens                      (over, (^.), (^..))
-import           Control.Monad.State               (evalStateT, get)
-import           Data.Data.Lens                    (uniplate)
-import           Data.Map.Strict                   (Map)
-import qualified Data.Map.Strict                   as Map
-import           Data.Maybe                        (catMaybes, fromMaybe)
-import qualified Data.Set                          as Set
-import           Data.Text                         (Text)
+import           Control.Arrow                      (first, second, (|||))
+import           Control.Lens                       (over, (^.), (^..))
+import           Control.Monad.State                (evalStateT, get)
+import           Data.Data.Lens                     (uniplate)
+import           Data.Map.Strict                    (Map)
+import qualified Data.Map.Strict                    as Map
+import           Data.Maybe                         (catMaybes, fromMaybe)
+import qualified Data.Set                           as Set
+import           Data.Text                          (Text)
 
-import           FuncDef                           (FuncDef (FuncDef))
-import           FuncId                            (FuncId (FuncId), name)
-import           FuncTable                         (FuncTable,
-                                                    Signature (Signature),
-                                                    toMap)
-import           Id                                (Id (Id))
-import           Sigs                              (Sigs, chan, func, pro,
-                                                    uniqueCombine)
-import qualified Sigs                              (empty)
-import           SortId                            (sortIdBool, sortIdInt,
-                                                    sortIdRegex, sortIdString)
-import           ChanId                 (ChanId)                 
-import           StdTDefs                          (stdFuncTable, stdTDefs)
-import           TxsDefs                           (TxsDefs, fromList, funcDefs, procDefs, modelDefs,
-                                                    union, ProcDef, ProcId)
-import qualified TxsDefs                           (empty)
-import           ValExpr                           (ValExpr,
-                                                    ValExprView (Vfunc, Vite),
-                                                    cstrITE, cstrVar, view)
-import           VarId                             (VarId)
+import           ChanId                             (ChanId)
+import           CstrId                             (CstrId)
+import           FuncDef                            (FuncDef (FuncDef))
+import           FuncId                             (FuncId (FuncId), name)
+import           FuncTable                          (FuncTable,
+                                                     Signature (Signature),
+                                                     toMap)
+import           Id                                 (Id (Id))
+import           Sigs                               (Sigs, chan, func, pro,
+                                                     uniqueCombine)
+import qualified Sigs                               (empty)
+import           SortId                             (sortIdBool, sortIdInt,
+                                                     sortIdRegex, sortIdString)
 import           SortId                             (SortId)
-import           CstrId (CstrId)
+import           StdTDefs                           (stdFuncTable, stdTDefs)
+import           TxsDefs                            (ProcDef, ProcId, TxsDefs,
+                                                     fromList, funcDefs,
+                                                     modelDefs, procDefs, union)
+import qualified TxsDefs                            (empty)
+import           ValExpr                            (ValExpr,
+                                                     ValExprView (Vfunc, Vite),
+                                                     cstrITE, cstrVar, view)
+import           VarId                              (VarId)
 
 import           TorXakis.Compiler.Data
 import           TorXakis.Compiler.Defs.ChanId
+import           TorXakis.Compiler.Defs.ProcDef
 import           TorXakis.Compiler.Defs.Sigs
 import           TorXakis.Compiler.Defs.TxsDefs
-import           TorXakis.Compiler.Error           (Error)
+import           TorXakis.Compiler.Error            (Error)
+import           TorXakis.Compiler.Maps
+import           TorXakis.Compiler.Maps.DefinesAMap
+import           TorXakis.Compiler.MapsTo
+import           TorXakis.Compiler.Simplifiable
+import           TorXakis.Compiler.ValExpr.Common
 import           TorXakis.Compiler.ValExpr.CstrId
 import           TorXakis.Compiler.ValExpr.ExpDecl
 import           TorXakis.Compiler.ValExpr.FuncDef
 import           TorXakis.Compiler.ValExpr.FuncId
 import           TorXakis.Compiler.ValExpr.SortId
 import           TorXakis.Compiler.ValExpr.VarId
-import           TorXakis.Compiler.MapsTo
-import           TorXakis.Compiler.Maps
-import           TorXakis.Compiler.Simplifiable
-import           TorXakis.Compiler.Defs.ProcDef
-import           TorXakis.Compiler.ValExpr.Common
-import           TorXakis.Compiler.Maps.DefinesAMap
 
-import TorXakis.Compiler.Data.ProcDecl
+import           TorXakis.Compiler.Data.ProcDecl
 
 import           TorXakis.Parser
 import           TorXakis.Parser.Data
@@ -83,79 +84,29 @@ compileLegacy = (throwOnLeft ||| id) . compile
 
 compileParsedDefs :: ParsedDefs -> CompilerM (Id, TxsDefs, Sigs VarId)
 compileParsedDefs pd = do
-    -- Construct the @SortId@'s lookup table.
-    sMap <- compileToSortId (pd ^. adts)
-    -- Construct the @CstrId@'s lookup table.
-    let pdsMap = Map.fromList [ ("Bool", sortIdBool)
-                              , ("Int", sortIdInt)
-                              , ("Regex", sortIdRegex)
-                              , ("String", sortIdString)
-                              ]
-        allSortsMap = Map.union pdsMap sMap
---    chs <- Map.fromList <$> chanDeclsToChanIds allSortsMap (pd ^. chdecls)
-    cMap <- compileToCstrId allSortsMap (pd ^. adts)
-    let --  e1 = e0 { cstrIdT = cMap }
-        allFuncs = pd ^. funcs ++ pd ^. consts
-    stdFuncIds <- getStdFuncIds
-    cstrFuncIds <- adtsToFuncIds allSortsMap (pd ^. adts)
-    -- Construct the variable declarations table.
-    let -- Predefined functions:
-        predefFuncs = funcDefInfoNamesMap $
-            (fst <$> stdFuncIds) ++ (fst <$> cstrFuncIds)
-        -- User defined functions:
-        userDefFuncs = Map.fromListWith (++) $
-                           zip (funcName <$> allFuncs)
-                               (return . FDefLoc . getLoc <$> allFuncs)
-        --  All the functions available at the top level. Note the union is
-        -- left biased, so functions defined by the user will have precedence
-        -- over predefined functions.
-        nameToFDefs = Map.unionWith (++) predefFuncs userDefFuncs
-        -- There are no variable declarations at the top level.
-        eVdMap = Map.empty :: Map Text (Loc VarDeclE) 
-    fRtoDs <- Map.fromList <$> mapRefToDecls (eVdMap :& nameToFDefs) allFuncs
-    pRtoDs <- Map.fromList <$> mapRefToDecls (eVdMap :& nameToFDefs) (pd ^. procs)
-    mRtoDs <- Map.fromList <$> mapRefToDecls (eVdMap :& nameToFDefs) (pd ^. models)
-    let dMap = fRtoDs `Map.union` pRtoDs `Map.union` mRtoDs
-    -- Construct the function declaration to function id table.
-    lFIdMap <- funcDeclsToFuncIds allSortsMap allFuncs
-    -- Join `lFIdMap` and  `stdFuncIds`.
-    let completeFidMap = Map.fromList $ --
-            fmap (first FDefLoc) (Map.toList lFIdMap)
-            ++ stdFuncIds
-            ++ cstrFuncIds
+    sIds <- compileToSortIds pd
+    cstrIds <- compileToCstrId sIds (pd ^. adts)
+    fIds <- compileToFuncIds sIds pd
+    let lfDefs = compileToFuncLocs fIds
+    decls <- compileToDecls lfDefs pd
     -- Infer the types of all variable declarations.
     let emptyVdMap = Map.empty :: Map (Loc VarDeclE) SortId
     -- We pass to 'inferTypes' an empty map from 'Loc VarDeclE' to 'SortId'
     -- since no variables can be declared at the top level.
-    vdSortMap <- inferTypes (allSortsMap :& dMap :& completeFidMap :& emptyVdMap) allFuncs
+    vdSortMap <- inferTypes (sIds :& decls :& fIds :& emptyVdMap) (allFuncs pd)
     -- Construct the variable declarations to @VarId@'s lookup table.
-    vMap <- generateVarIds (vdSortMap :& completeFidMap ) allFuncs
-    lFDefMap <- funcDeclsToFuncDefs (vMap :& completeFidMap :& dMap) allFuncs
-     -- Construct the @ProcId@ to @ProcDef@ map:
-    -- pVDecls <- getMap allSortsMap (pd ^. procs)
-    --            :: CompilerM (Map (Loc ProcDeclE) [(Loc VarDeclE, VarId)])
-    -- pCDecls <- getMap allSortsMap (pd ^. procs)
-    --            :: CompilerM (Map (Loc ProcDeclE) [(Loc ChanDeclE, ChanId)])
-    pdefMap <- procDeclsToProcDefMap
-                 ( allSortsMap
-                 :& cMap
-                 :& completeFidMap
-                 :& lFDefMap
-                 :& dMap
---                 :& (pCDecls)
-                 )
-                 (pd ^. procs)
-    -- Finally construct the TxsDefs.
-    let mm = allSortsMap :& pdefMap :& cMap :& completeFidMap :& lFDefMap
---    chDecls <- getMap () pd :: CompilerM (Map (Loc ChanRefE) (Loc ChanDeclE))
---    chIds   <- getMap allSortsMap pd :: CompilerM (Map (Loc ChanDeclE) ChanId)
-    chIds   <- getMap allSortsMap (pd ^. chdecls) :: CompilerM (Map (Loc ChanDeclE) ChanId)
-    sigs    <- toSigs                (mm :& chIds) pd
+    vIds <- generateVarIds (vdSortMap :& fIds ) (allFuncs pd)
+    fdefs <- funcDeclsToFuncDefs (vIds :& fIds :& decls) (allFuncs pd)
+    pdefs <- procDeclsToProcDefMap (sIds :& cstrIds :& fIds :& fdefs :& decls)
+                                   (pd ^. procs)
+    chIds   <- getMap sIds (pd ^. chdecls) :: CompilerM (Map (Loc ChanDeclE) ChanId)
+    let mm = sIds :& pdefs :& cstrIds :& fIds :& fdefs
+    sigs    <- toSigs (mm :& chIds) pd
     -- We need the map from channel names to the locations in which these
     -- channels are declared, because the model definitions rely on channels
     -- declared outside its scope.
     chNames <-  getMap () (pd ^. chdecls) :: CompilerM (Map Text (Loc ChanDeclE))
-    txsDefs <- toTxsDefs (func sigs) (mm :& dMap :& vMap :& vdSortMap :& chNames :& chIds) pd
+    txsDefs <- toTxsDefs (func sigs) (mm :& decls :& vIds :& vdSortMap :& chNames :& chIds) pd
     St i    <- get
     return (Id i, txsDefs, sigs)
 
@@ -163,10 +114,9 @@ toTxsDefs :: ( MapsTo Text        SortId mm
              , MapsTo (Loc CstrE) CstrId mm
              , MapsTo (Loc VarRefE) (Either (Loc VarDeclE) [FuncDefInfo]) mm
              , MapsTo FuncDefInfo FuncId mm
-             , MapsTo FuncId (FuncDef VarId) mm 
+             , MapsTo FuncId (FuncDef VarId) mm
              , MapsTo ProcId ProcDef mm
              , MapsTo Text (Loc ChanDeclE) mm
-             -- , MapsTo (Loc ChanRefE) (Loc ChanDeclE) mm
              , MapsTo (Loc ChanDeclE) ChanId mm
              , MapsTo (Loc VarDeclE) VarId mm
              , MapsTo (Loc VarDeclE) SortId mm
@@ -187,13 +137,13 @@ toTxsDefs ft mm pd = do
             }
         pds = TxsDefs.empty {
             procDefs = simplify ft fn (innerMap mm)
-            }    
+            }
     mDefMap <- modelDeclsToTxsDefs mm (pd ^. models)
     let mds = TxsDefs.empty { modelDefs = simplify ft fn mDefMap }
     return $ ads
         `union` fds
-        `union` pds        
-        `union` fromList stdTDefs        
+        `union` pds
+        `union` fromList stdTDefs
         `union` mds
 
 toSigs :: ( MapsTo Text        SortId mm
@@ -227,3 +177,59 @@ funcDefInfoNamesMap fdis =
       asPair fdi = (, fdi) <$> fdiName fdi
       groupByName :: [(Text, FuncDefInfo)] -> Map Text [FuncDefInfo]
       groupByName = Map.fromListWith (++) . fmap (second pure)
+
+-- | Get a dictionary from sort names to their @SortId@. The sorts returned
+-- include all the sorts defined by a 'TYPEDEF' (in the parsed definitions),
+-- and the predefined sorts ('Bool', 'Int', 'Regex', 'String').
+compileToSortIds :: ParsedDefs -> CompilerM (Map Text SortId)
+compileToSortIds pd = do
+    -- Construct the @SortId@'s lookup table.
+    sMap <- compileToSortId (pd ^. adts)
+    let pdsMap = Map.fromList [ ("Bool", sortIdBool)
+                              , ("Int", sortIdInt)
+                              , ("Regex", sortIdRegex)
+                              , ("String", sortIdString)
+                              ]
+    return $ Map.union pdsMap sMap
+
+-- | Get a dictionary from locations of function definitions (included
+-- locations of predefined functions), to their function id's.
+compileToFuncIds :: Map Text SortId ->  ParsedDefs -> CompilerM (Map FuncDefInfo FuncId)
+compileToFuncIds sIds pd = do
+    stdFuncIds <- getStdFuncIds
+    cstrFuncIds <- adtsToFuncIds sIds (pd ^. adts)
+    -- Construct the function declaration to function id table.
+    lFIdMap <- funcDeclsToFuncIds sIds (allFuncs pd)
+    -- Join `lFIdMap` and  `stdFuncIds`.
+    return $ Map.fromList
+        $  fmap (first FDefLoc) (Map.toList lFIdMap) -- TODO: why not having `funcDeclsToFuncIds` return a list?
+        ++ stdFuncIds
+        ++ cstrFuncIds
+
+-- | Get all the functions in the parsed definitions.
+allFuncs :: ParsedDefs -> [FuncDecl]
+allFuncs pd = pd ^. funcs ++ pd ^. consts
+
+-- | Get a dictionary from the function names to the locations in which these
+-- functions are defined.
+--
+compileToFuncLocs :: Map FuncDefInfo FuncId -> Map Text [FuncDefInfo]
+compileToFuncLocs fIds = Map.fromListWith (++) $
+    fmap mkPair (Map.toList fIds)
+    where
+      mkPair :: (FuncDefInfo, FuncId) -> (Text, [FuncDefInfo])
+      mkPair (fdi, fId) = (name fId, [fdi])
+
+-- | Get a dictionary from variable references to the possible location in
+-- which these variables are declared. Due to overloading a syntactic reference
+-- to a variable can refer to a variable, or multiple functions.
+compileToDecls :: Map Text [FuncDefInfo]
+               -> ParsedDefs
+               -> CompilerM (Map (Loc VarRefE) (Either (Loc VarDeclE) [FuncDefInfo]))
+compileToDecls lfDefs pd = do
+    let eVdMap = Map.empty :: Map Text (Loc VarDeclE)
+    fRtoDs <- Map.fromList <$> mapRefToDecls (eVdMap :& lfDefs) (allFuncs pd)
+    pRtoDs <- Map.fromList <$> mapRefToDecls (eVdMap :& lfDefs) (pd ^. procs)
+    mRtoDs <- Map.fromList <$> mapRefToDecls (eVdMap :& lfDefs) (pd ^. models)
+    return $ fRtoDs `Map.union` pRtoDs `Map.union` mRtoDs
+
