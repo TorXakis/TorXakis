6948da8d915fe4813e441563f08c15c1d971a14a
Kerem Ispirli <kerem.ispirli@tno.nl>
2018-06-11 09:43:44 +0200

    Added help text for tester and test

--- sys/txs-cli/src/TorXakis/CLI/Help.hs
+++ sys/txs-cli/src/TorXakis/CLI/Help.hs
@@ -10,24 +10,29 @@ helpText = [r|
 TorXakis :: Model-Based Testing
 --------------------------------
 
-quit, q                         : stop TorXakis completely
-exit, x                         : exit the current command run of TorXakis
-help, h, ?                      : show help (this text)
-info, i                         : show info on TorXakis
-seed <n>                        : set random seed to <n>
+quit, q                        : stop TorXakis completely
+exit, x                        : exit the current command run of TorXakis
+help, h, ?                     : show help (this text)
+info, i                        : show info on TorXakis
+seed <n>                       : set random seed to <n>
 
-delay <n>                       : wait for <n> seconds
-echo <text>                     : echo the input <text>
-# <text>                        : comment, <text> is ignored
+delay <n>                      : wait for <n> seconds
+echo <text>                    : echo the input <text>
+# <text>                       : comment, <text> is ignored
 
-var [<variable-declarations>]    : show/[declare] variables
-val [<value-definitions>]        : show/[define] values
-eval <value-expression>          : evaluate the (closed) <value-expression>
+var [<variable-declarations>]  : show/[declare] variables
+val [<value-definitions>]      : show/[define] values
+eval <value-expression>        : evaluate the (closed) <value-expression>
 
-load <file path>                : load TorXakis model definitions to the session
-stepper <model>                 : start stepping with model <model> where <model>
-                                  is a model in model definitions which has been
-                                  loaded with `load` command
-step <action>                   : make a step identified by <action>
-step [<n>]                      : make <n>/1 random steps
+load <file path>               : load TorXakis model definitions to the session
+tester <model> [<purpose>]     : start testing with model <mod> and connection <cnect>
+       [<mapper>] <cnect>        using possible test purpose <purp> and/or mapper <map>
+test <action>                  : make a test step identified by (visible) input <action>
+test                           : make a test step by observing output/quiescence
+test <n>                       : make <n> random test steps
+stepper <model>                : start stepping with model <model> where <model>
+                                 is a model in model definitions which has been
+                                 loaded with `load` command
+step <action>                  : make a step identified by <action>
+step [<n>]                     : make <n>/1 random steps
 |]
