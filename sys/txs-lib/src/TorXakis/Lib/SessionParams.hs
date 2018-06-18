1cd56dd8255fccc5d19ce5c12f3344b9b23efec7
Kerem Ispirli <kerem.ispirli@tno.nl>
2018-06-18 10:16:32 +0200

    Updated validation of SessionParams as in ParamServer

--- sys/txs-lib/src/TorXakis/Lib/SessionParams.hs
+++ sys/txs-lib/src/TorXakis/Lib/SessionParams.hs
@@ -11,12 +11,15 @@ import qualified Data.Map  as Map
 
 import           ParamCore (Params)
 
+positiveInt :: String -> Bool
+positiveInt s = not (null s) && all Char.isDigit s && (let v = read s :: Integer in (v <= toInteger (maxBound :: Int) ) )
+
 initSessionParams :: Params
 initSessionParams  =  Map.fromList $ map ( \(x,y,z) -> (x,(y,z)) )
-    [ ( "param_Sut_deltaTime"      , "2000"      , \s -> not (null s) && all Char.isDigit s)
+    [ ( "param_Sut_deltaTime"      , "2000"      , positiveInt)
             -- param_Sut_deltaTime :: Int (>0)
             -- quiescence output time (millisec >0)
-    , ( "param_Sim_deltaTime"      , "2000"       , \s -> not (null s) && all Char.isDigit s)
+    , ( "param_Sim_deltaTime"      , "2000"       , positiveInt)
             -- param_Sim_deltaTime :: Int (>0)
             -- quiescence input time (millisec >0)
     ]
