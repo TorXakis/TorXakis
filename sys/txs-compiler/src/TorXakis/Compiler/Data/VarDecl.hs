287e5075386d721a433d537d9fab8e14884044cd
Damian Nadales <damian.nadales@gmail.com>
2018-06-20 16:57:20 +0200

    Cleanup `TorXakis.Compiler.Data.VarDecl`.

--- sys/txs-compiler/src/TorXakis/Compiler/Data/VarDecl.hs
+++ sys/txs-compiler/src/TorXakis/Compiler/Data/VarDecl.hs
@@ -1,33 +1,40 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TupleSections         #-}
--- TODO: We are not exporting 'DefinesAMap' outside the compiler, so the
--- declaration of orphan instances is not likely to cause problems. As an
--- alternative we can move the 'ProcDecl' parser data type here. However I
--- don't know how good that design decision is, since the parser and the
--- compiler are sharing the same module.
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  TorXakis.Compiler.Data.VarDecl
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+--
+-- Maintainer  :  damian.nadales@gmail.com (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Compiler functions and instances on variable declarations.
+--------------------------------------------------------------------------------
 {-# OPTIONS_GHC -Wno-orphans #-}
-module TorXakis.Compiler.Data.VarDecl where
+module TorXakis.Compiler.Data.VarDecl
+    ()
+where
 
-import           Control.Lens                       ((^.), (^..))
-import           Data.Data.Lens                     (biplate)
-
-
-import           Data.Map                           (Map)
-import qualified Data.Map                           as Map
 import           Data.Text                          (Text)
 
 import           SortId                             (SortId)
-import           VarId                              (VarId (VarId))
+import           VarId                              (VarId)
 
-import           TorXakis.Compiler.Data
-import           TorXakis.Compiler.Maps
-import           TorXakis.Compiler.Maps.DefinesAMap
-import           TorXakis.Compiler.MapsTo
-import           TorXakis.Compiler.ValExpr.SortId
-import           TorXakis.Compiler.ValExpr.VarId
-import           TorXakis.Parser.Data
+import           TorXakis.Compiler.Maps             ((.@!!))
+import           TorXakis.Compiler.Maps.DefinesAMap (DefinesAMap, uGetKVs)
+import           TorXakis.Compiler.MapsTo           (MapsTo)
+import           TorXakis.Compiler.ValExpr.VarId    (varIdFromVarDecl)
+import           TorXakis.Parser.Data               (Loc, VarDecl, VarDeclE,
+                                                     getLoc, varDeclSort)
 
 instance ( MapsTo Text SortId mm
          ) => DefinesAMap (Loc VarDeclE) SortId VarDecl mm where
