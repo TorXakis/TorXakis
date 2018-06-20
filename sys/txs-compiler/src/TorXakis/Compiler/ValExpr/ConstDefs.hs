d2c49b682cf96bdd4df1bb0848f582bfca1850d1
Damian Nadales <damian.nadales@gmail.com>
2018-06-20 16:21:57 +0200

    Cleanup `ConstDefs`.

--- sys/txs-compiler/src/TorXakis/Compiler/ValExpr/ConstDefs.hs
+++ sys/txs-compiler/src/TorXakis/Compiler/ValExpr/ConstDefs.hs
@@ -1,12 +1,28 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+--------------------------------------------------------------------------------
 -- |
-
+-- Module      :  TorXakis.Compiler.ValExpr.ConstDefs
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+--
+-- Maintainer  :  damian.nadales@gmail.com (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Compilation to 'TorXakis' @Const@'s.
+--------------------------------------------------------------------------------
 module TorXakis.Compiler.ValExpr.ConstDefs where
 
 import qualified ConstDefs
 import           SortId               (SortId)
 
-import           TorXakis.Parser.Data
+import           TorXakis.Parser.Data (Const (AnyConst, BoolConst, IntConst, RegexConst, StringConst))
 
+-- | Transform a constant declaration into a 'TorXakis' @Const@.
 constToConstDef :: SortId -> Const -> ConstDefs.Const
 constToConstDef _ (BoolConst b)   = ConstDefs.Cbool b
 constToConstDef _ (IntConst i)    = ConstDefs.Cint i
