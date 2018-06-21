9f995d6991b16edd096d90ba586b49127d65a862
Damian Nadales <damian.nadales@gmail.com>
2018-06-21 16:26:53 +0200

    Cleanup `Parser/ConstDecl`. [skip ci]

--- sys/txs-compiler/src/TorXakis/Parser/ConstDecl.hs
+++ sys/txs-compiler/src/TorXakis/Parser/ConstDecl.hs
@@ -1,6 +1,23 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+--------------------------------------------------------------------------------
 -- |
-
-module TorXakis.Parser.ConstDecl where
+-- Module      :  TorXakis.Parser.ConstDecl
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+--
+-- Maintainer  :  damian.nadales@gmail.com (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Parser for constant declarations.
+--------------------------------------------------------------------------------
+module TorXakis.Parser.ConstDecl
+    (constDeclsP)
+where
 
 import           Text.Parsec                 (sepBy)
 
@@ -9,6 +26,7 @@ import           TorXakis.Parser.Data
 import           TorXakis.Parser.TypeDefs
 import           TorXakis.Parser.ValExprDecl
 
+-- | Parse a constant declaration. Constants are treated as nullary functions.
 constDeclsP :: TxsParser [FuncDecl]
 constDeclsP = do
     txsSymbol "CONSTDEF"
