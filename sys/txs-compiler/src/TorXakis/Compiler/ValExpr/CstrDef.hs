68e85979c11e1f59627d716ba8f0cdf70c035b0c
Damian Nadales <damian.nadales@gmail.com>
2018-06-21 13:44:48 +0200

    Cleanup `TorXakis/Compiler/ValExpr/CstrDef.hs` [skip ci].

--- sys/txs-compiler/src/TorXakis/Compiler/ValExpr/CstrDef.hs
+++ sys/txs-compiler/src/TorXakis/Compiler/ValExpr/CstrDef.hs
@@ -1,33 +1,59 @@
-{-# LANGUAGE FlexibleContexts  #-}
-module TorXakis.Compiler.ValExpr.CstrDef where
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+{-# LANGUAGE FlexibleContexts #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  TorXakis.Compiler.ValExpr.CstrDef
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+--
+-- Maintainer  :  damian.nadales@gmail.com (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Compilation functions related to 'TorXakis' constructor definitions.
+--------------------------------------------------------------------------------
+module TorXakis.Compiler.ValExpr.CstrDef
+    (compileToCstrDefs)
+where
 
-import Prelude hiding (lookup)
-import           Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Text (Text)
+import           Data.Map                         (Map)
+import qualified Data.Map                         as Map
+import           Data.Text                        (Text)
 
-import           SortId (SortId)
-import           CstrId (CstrId)
-import           CstrDef (CstrDef (CstrDef))
+import           CstrDef                          (CstrDef (CstrDef))
+import           CstrId                           (CstrId)
+import           SortId                           (SortId)
 
-import           TorXakis.Parser.Data
-import           TorXakis.Compiler.Data
-import           TorXakis.Compiler.MapsTo
-import           TorXakis.Compiler.Maps
-import           TorXakis.Compiler.ValExpr.FuncId
-    
+import           TorXakis.Compiler.Data           (CompilerM)
+import           TorXakis.Compiler.MapsTo         (MapsTo, lookupM)
+import           TorXakis.Compiler.ValExpr.FuncId (cstrToAccFuncId,
+                                                   cstrToIsCstrFuncId)
+import           TorXakis.Parser.Data             (ADTDecl, CstrDecl, CstrE,
+                                                   Loc, constructors,
+                                                   cstrFields, getLoc)
+
+-- | Compile a list of ADT declarations into a map from constructor id's to
+-- their definitions.
 compileToCstrDefs :: ( MapsTo Text        SortId mm
                      , MapsTo (Loc CstrE) CstrId mm )
                   => mm -> [ADTDecl] -> CompilerM (Map CstrId CstrDef)
-compileToCstrDefs mm ds = 
+compileToCstrDefs mm ds =
     Map.fromList . concat <$> traverse (adtToCstrDefs mm) ds
 
+-- | Compile an ADT declaration into a list constructor id's and
+-- constructor definition pairs.
 adtToCstrDefs :: ( MapsTo Text        SortId mm
                  , MapsTo (Loc CstrE) CstrId mm )
                => mm -> ADTDecl -> CompilerM [(CstrId, CstrDef)]
 adtToCstrDefs mm a =
     traverse (cstrToCstrDefs mm) (constructors a)
 
+-- | Compile a constructor declaration into a list constructor id's and
+-- constructor definition pairs.
 cstrToCstrDefs :: ( MapsTo Text        SortId mm
                   , MapsTo (Loc CstrE) CstrId mm )
                => mm -> CstrDecl -> CompilerM (CstrId, CstrDef)
