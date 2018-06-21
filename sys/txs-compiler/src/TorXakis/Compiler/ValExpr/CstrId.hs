a82b6b0e28fcfdd632caef329d3a6c1dc34f6cf3
Damian Nadales <damian.nadales@gmail.com>
2018-06-21 13:50:00 +0200

    Cleanup `TorXakis/Compiler/ValExpr/CstrId.hs` [skip ci]

--- sys/txs-compiler/src/TorXakis/Compiler/ValExpr/CstrId.hs
+++ sys/txs-compiler/src/TorXakis/Compiler/ValExpr/CstrId.hs
@@ -1,25 +1,42 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts  #-}
-module TorXakis.Compiler.ValExpr.CstrId where
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+{-# LANGUAGE FlexibleContexts #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  TorXakis.Compiler.ValExpr.CstrId
+-- Copyright   :  (c) TNO and Radboud University
+-- License     :  BSD3 (see the file license.txt)
+--
+-- Maintainer  :  damian.nadales@gmail.com (Embedded Systems Innovation by TNO)
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Compilation functions related to 'TorXakis' constructor id's.
+--------------------------------------------------------------------------------
+module TorXakis.Compiler.ValExpr.CstrId
+    (compileToCstrId)
+where
 
-import Prelude hiding (lookup)
-import           Control.Arrow             (left)
-import           Control.Monad.Error.Class (liftEither)
-import           Data.Map                  (Map)
-import qualified Data.Map                  as Map
-import           Data.Semigroup            ((<>))
-import Data.Text (Text)
+import           Data.Map                 (Map)
+import qualified Data.Map                 as Map
+import           Data.Text                (Text)
 
-import           CstrId                    (CstrId (CstrId))
-import           Id                        (Id (Id))
-import           SortId                    (SortId)
+import           CstrId                   (CstrId (CstrId))
+import           Id                       (Id (Id))
+import           SortId                   (SortId)
 
-import           TorXakis.Compiler.Data
-import           TorXakis.Compiler.Maps
-import           TorXakis.Compiler.MapsTo
-import           TorXakis.Compiler.Error
-import           TorXakis.Parser.Data
+import           TorXakis.Compiler.Data   (CompilerM, getNextId)
+import           TorXakis.Compiler.Maps   (findSortIdM)
+import           TorXakis.Compiler.MapsTo (MapsTo)
+import           TorXakis.Parser.Data     (ADTDecl, CstrDecl, CstrE, Loc,
+                                           adtName, constructors, cstrFields,
+                                           cstrName, fieldSort, getLoc, nodeLoc)
 
+-- | Compile a list of ADT declarations into a map from the location of the
+-- constructor declaration, to their corresponding constructor id's.
 compileToCstrId :: (MapsTo Text SortId mm)
                 => mm -> [ADTDecl] -> CompilerM (Map (Loc CstrE) CstrId)
 compileToCstrId mm ds = Map.fromList . concat <$>
@@ -42,9 +59,3 @@ cstrToCstrId mm sId c = do
     i <- getNextId
     aSids <- traverse (findSortIdM mm . fieldSort) (cstrFields c)
     return (getLoc c, CstrId (cstrName c) (Id i) aSids sId)
-
-cstrIdOfCstrDecl :: MapsTo (Loc CstrE) CstrId mm => mm -> CstrDecl -> Either Error CstrId
-cstrIdOfCstrDecl mm c = lookup (getLoc c) mm
-
-cstrIdOfCstrDeclM :: MapsTo (Loc CstrE) CstrId mm => mm -> CstrDecl -> CompilerM CstrId
-cstrIdOfCstrDeclM mm c = liftEither $ cstrIdOfCstrDecl mm c
