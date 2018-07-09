d7aa635bc415d8780108dc019059526955c7fc39
pjljvandelaar <pierre.vandelaar@tno.nl>
2018-07-09 21:12:49 +0200

    Update setup.sh

--- ci/setup.sh
+++ ci/setup.sh
@@ -45,7 +45,8 @@ else
     Z3ZIP=$(ls $CURDIR/z3-*.zip)
     unzip $Z3ZIP
     Z3NAME=$(ls z3-*)
-    mv $Z3NAME z3/
+    echo $Z3NAME
+    mv -v $Z3NAME ./z3/
     chmod +x ./z3/bin
     touch ./z3/z3-4.7.1
     cd $CURDIR
