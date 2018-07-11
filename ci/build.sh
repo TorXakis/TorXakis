08319253399b915cd5f5a8f627f3cc32182227a1
pjljvandelaar <pierre.vandelaar@tno.nl>
2018-07-11 13:01:11 +0200

    Moved to new versions of cvc4 and z3 (#764)
    * Moved to new versions of cvc4 and z3
    
    * script contained flaws -- repaired
    
    * new z3 has an additional directory
    
    * mv is rename and move
    
    * New cvc4 has changed flags
          As already written down: https://github.com/TorXakis/TorXakis/issues/652
         cvc4 has changed flags.
         Had to be changed in THREE locations.
    
    *  more info on setting and versions
    
    *  link incorrect - Not found message made executable ;-)
    
    * changed stack.yaml to clean caches on appveyor

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,13 @@
 # See LICENSE at root directory of this repository.
 
 # Stack should be installed at this stage.
-echo "Using stack `stack --version`"
+
+echo $PATH
+echo "stack"
+stack --version
+echo "cvc4"
+cvc4 --version
+echo "z3"
+z3 -version
 
 stack install --pedantic --install-ghc --haddock --test --stack-yaml stack_linux.yaml --work-dir $CACHE_DIR_REL/.stack-work --stack-root $CACHE_DIR/.stack --allow-different-user
--- ci/setup.sh
+++ ci/setup.sh
@@ -2,6 +2,7 @@
 # Copyright (c) 2015-2017 TNO and Radboud University
 # See LICENSE at root directory of this repository.
 
+
 set -e
 
 javac ./test/copyright/Copyright.java
@@ -21,30 +22,30 @@ else
     curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C $CACHE_DIR/bin '*/stack'
 fi
 
-if [ -f $CACHE_DIR/bin/cvc4 ] && [ -e $CACHE_DIR/bin/cvc4-20180306 ]
+if [ -f $CACHE_DIR/bin/cvc4 ] && [ -e $CACHE_DIR/bin/cvc4-1.6a ]
 then
     echo "$CACHE_DIR/bin/cvc4 found in cache."
 else
-    echo "cvc4 not found in cache or different version than 20180306"
+    echo "cvc4 not found in cache or different version than 1.6"
     rm -f $CACHE_DIR/bin/cvc4*
-    curl -L https://github.com/TorXakis/Dependencies/releases/download/cvc4-20180306/cvc4-20180306-x86_64-linux-opt -o cvc4
+    curl -L https://github.com/TorXakis/Dependencies/releases/download/cvc4_1.6/cvc4-1.6-x86_64-linux-opt -o cvc4
     mv cvc4 $CACHE_DIR/bin
     chmod +x $CACHE_DIR/bin/cvc4
-    touch $CACHE_DIR/bin/cvc4-20180306
+    touch $CACHE_DIR/bin/cvc4-1.6
 fi
 
-if [ -d $CACHE_DIR/z3 ] && [ -e $CACHE_DIR/z3/z3-4.6.0 ]
+if [ -d $CACHE_DIR/z3 ] && [ -e $CACHE_DIR/z3/z3-4.7.1 ]
 then
-    echo "$CACHE_DIR/z3 build 4.6.0 found in cache."
+    echo "$CACHE_DIR/z3 build 4.7.1 found in cache."
 else
-    echo "z3 not found in cache or different version than 4.6.0"
+    echo "z3 not found in cache or different version than 4.7.1"
     rm $CACHE_DIR/z3 -rf
-    curl -L -O https://github.com/TorXakis/Dependencies/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-14.04.zip
-    CURDIR=$(pwd)
-    mkdir $CACHE_DIR/z3 && cd $CACHE_DIR/z3
-    Z3ZIP=$(ls $CURDIR/z3*.zip)
-    unzip $Z3ZIP
-    chmod +x ./bin/z3
-    touch ./z3-4.6.0
-    cd $CURDIR
+    curl -L https://github.com/TorXakis/Dependencies/releases/download/z3-4.7.1/z3-4.7.1-x64-ubuntu-14.04.zip -o z3.zip
+    unzip z3.zip
+    Z3NAME=$(ls -d z3-*/)
+    echo $Z3NAME
+    mv $Z3NAME $CACHE_DIR/z3
+    chmod +x $CACHE_DIR/z3/bin/z3
+    touch $CACHE_DIR/z3/z3-4.7.1
 fi
+echo "Set up done"
