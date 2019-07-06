3795e02903c736e86a6361bed6f62a8ac573bb2c
Damian Nadales <damian.nadales@iohk.io>
2019-07-06 12:47:08 +0200

    - Add a docker file for building UBUNTU 16.04 packages. - Add a `mk-package.sh` script for creating the TorXakis package.
    [skip ci]

---
+++ ci/mk-package/ubuntu/README.md
@@ -0,0 +1,22 @@
+
+[Docker installation instructions](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04)
+
+Remember to run:
+
+```sh
+newgrp docker
+```
+
+To be able to use Docker without sudo without logging out.
+
+Build:
+
+```sh
+docker build --tag=torxakis-packager:16.04 -f 16.04/Dockerfile .
+```
+
+Run:
+
+```sh
+docker run --rm -e TXS_VERSION=0.8.1 -e UBUNTU_VERSION=16.04 -v /tmp/build:/build -ti torxakis-packager:16.04 mk-package.sh
+```
---
+++ ci/mk-package/ubuntu/mk-package.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+git --version
+stack --version
+
+echo "Cloning TorXakis"
+
+cd /build
+
+echo "Hello from docker" >> hello.txt
+
+if [ -d /build/TorXakis ]
+then
+    echo "TorXakis already cloned"
+else
+    git clone https://github.com/TorXakis/TorXakis.git
+fi
+
+cd TorXakis
+
+git fetch origin
+
+git checkout $TXS_VERSION
+
+git pull origin $TXS_VERSION
+
+# We will be using a stack configuration file that is linux specific
+export STACK_YAML="stack_linux.yaml"
+
+# stack build --fast
+
+TXS_TMP=/build
+# rm -fr $TXS_TMP
+mkdir -p ${TXS_TMP}/usr/bin
+
+# echo "Temporary directory is: ${TXS_TMP}"
+
+TXS_BIN=`stack path --local-install-root --silent --stack-yaml stack_linux.yaml`/bin
+
+cp ${TXS_BIN}/* ${TXS_TMP}/usr/bin/
+
+# cd $TXS_TMP
+
+export CACHE_DIR="/build"
+
+echo "Looking for cvc4 in ${CACHE_DIR}/usr/bin/)"
+
+if [ -f $CACHE_DIR/usr/bin/cvc4 ] && [ -e $CACHE_DIR/usr/bin/cvc4-1.7 ]
+then
+    echo "${CACHE_DIR}/usr/bin/cvc4 found in cache."
+else
+    echo "cvc4 not found in cache or different version than 1.7"
+    rm -f $CACHE_DIR/usr/bin/cvc4*
+    curl -L https://github.com/TorXakis/Dependencies/releases/download/cvc4_1.7/cvc4-1.7-x86_64-linux-opt -o $CACHE_DIR/usr/bin/cvc4
+    chmod +x $CACHE_DIR/usr/bin/cvc4
+    touch $CACHE_DIR/usr/bin/cvc4-1.7
+fi
+
+if [ -f $CACHE_DIR/usr/bin/z3 ] && [ -e $CACHE_DIR/usr/bin/z3-4.8.5 ]
+then
+    echo "$CACHE_DIR/usr/bin/z3 found in cache."
+else
+    echo "z3 not found in cache or different version than z3-4.8.5"
+    rm $CACHE_DIR/usr/bin/z3 -rf
+    curl -L https://github.com/TorXakis/Dependencies/releases/download/z3-4.8.5/z3-4.8.5-x64-ubuntu-14.04.zip -o z3.zip
+    unzip -p z3.zip "*/bin/z3" > $CACHE_DIR/usr/bin/z3
+    chmod +x $CACHE_DIR/usr/bin/z3
+    touch $CACHE_DIR/usr/bin/z3-4.8.5
+fi
+
+# # Download and copy the Z3 binary
+# curl -L https://github.com/TorXakis/Dependencies/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-14.04.zip -o z3.zip
+# unzip -p z3.zip "bin/z3" > usr/bin/z3
+# chmod +x usr/bin/z3
+
+# cd -
+
+# # Create the deb package
+# fpm -s dir \
+#     -t deb \
+#     -n torxakis \
+#     -v $TXS_VERSION \
+#     -C $TXS_TMP \
+#     -d "libgmp-dev" \
+#     -d "libexpat1" \
+#     -d "netbase" \
+#     -d "libgomp1" \
+#     -p torxakis_VERSION_ARCH.deb \
+#     usr/bin
+
+# # Create an rpm from the deb package
+echo "Creating torxakis_${TXS_VERSION}-ubuntu_${UBUNTU_VERSION}-amd64.deb"
+
+# fpm -t rpm -s deb torxakis_${TXS_VERSION}-ubuntu_${UBUNTU_VERSION}-amd64.deb
+
+echo "Done!"
