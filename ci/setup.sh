12cb5db5ebe87fe2b8218a2aa73f9ef57d64e6a5
pjljvandelaar <pierre.vandelaar@tno.nl>
2020-01-07 09:03:21 +0100

    newer version for TorXakis v0.9.1

--- ci/setup.sh
+++ ci/setup.sh
@@ -15,13 +15,13 @@ then
     mkdir -p $CACHE_DIR/bin
 fi
 
-if [ -f $CACHE_DIR/bin/stack ] && [ -e $CACHE_DIR/bin/stack-2.1.1 ]
+if [ -f $CACHE_DIR/bin/stack ] && [ -e $CACHE_DIR/bin/stack-2.1.3 ]
 then
     echo "$CACHE_DIR/bin/stack found in cache."
 else
-    echo "stack not found in cache or different version than 2.1.1."
+    echo "stack not found in cache or different version than 2.1.3."
     rm -f $CACHE_DIR/bin/stack*
-    curl -L https://github.com/commercialhaskell/stack/releases/download/v2.1.1/stack-2.1.1-linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C $CACHE_DIR/bin '*/stack'
+    curl -L https://github.com/commercialhaskell/stack/releases/download/v2.1.3/stack-2.1.3-linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C $CACHE_DIR/bin '*/stack'
     touch $CACHE_DIR/bin/stack-2.1.1
 fi
 
@@ -37,18 +37,18 @@ else
     touch $CACHE_DIR/bin/cvc4-1.7
 fi
 
-if [ -d $CACHE_DIR/z3 ] && [ -e $CACHE_DIR/z3/z3-4.8.5 ]
+if [ -d $CACHE_DIR/z3 ] && [ -e $CACHE_DIR/z3/z3-4.8.7 ]
 then
-    echo "$CACHE_DIR/z3 build z3-4.8.5found in cache."
+    echo "$CACHE_DIR/z3 build z3-4.8.7 found in cache."
 else
-    echo "z3 not found in cache or different version than z3-4.8.5"
+    echo "z3 not found in cache or different version than z3-4.8.7"
     rm $CACHE_DIR/z3 -rf
-    curl -L https://github.com/TorXakis/Dependencies/releases/download/z3-4.8.5/z3-4.8.5-x64-ubuntu-14.04.zip -o z3.zip
+    curl -L https://github.com/TorXakis/Dependencies/releases/download/z3-4.8.7/z3-4.8.7-x64-ubuntu-16.04.zip -o z3.zip
     unzip z3.zip
     Z3NAME=$(ls -d z3-*/)
     echo $Z3NAME
     mv $Z3NAME $CACHE_DIR/z3
     chmod +x $CACHE_DIR/z3/bin/z3
-    touch $CACHE_DIR/z3/z3-4.8.5
+    touch $CACHE_DIR/z3/z3-4.8.7
 fi
 echo "Set up done"
