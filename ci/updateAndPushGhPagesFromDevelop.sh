ccb438b0a4a186412aa65531dacd6ce40f40fe0c
pjljvandelaar <pierre.vandelaar@tno.nl>
2020-03-24 14:17:39 +0100

    removed unnecessary separators (;)

--- ci/updateAndPushGhPagesFromDevelop.sh
+++ ci/updateAndPushGhPagesFromDevelop.sh
@@ -7,20 +7,20 @@ git branch -D gh-pages
 #remove gh-pages remotely
 git push origin --delete gh-pages
 #create new gh-pages branch
-git checkout -b gh-pages;
+git checkout -b gh-pages
 #generated documentation
 DOC_DIR=$(stack --work-dir .semaphore-cache/.stack-work --stack-root /home/runner/TorXakis/.semaphore-cache/.stack --allow-different-user --stack-yaml stack_linux.yaml path --local-doc-root)
 echo $DOC_DIR
 ls $DOC_DIR
 #copy documentation (ensure doc directory is empty first)
 rm ./doc/* -rf
-cp -r $DOC_DIR/. ./doc/;
+cp -r $DOC_DIR/. ./doc/
 #add theme for github pages
 echo "theme: jekyll-theme-slate" > _config.yml
 #add changes to gh-pages branch
-git add .;
-commitMsg="Haddock @ $(date +%Y%m%d_%H%M%S)";
+git add .
+commitMsg="Haddock @ $(date +%Y%m%d_%H%M%S)"
 #commit changes to gh-pages branch
-git commit -m "$(echo $commitMsg)";
+git commit -m "$(echo $commitMsg)"
 #push changes to gh-pages branch to remote server
-git push --set-upstream origin -f "https://torxakis-admin:$GITHUB_TOKEN@github.com/TorXakis/TorXakis.git";
+git push --set-upstream origin -f "https://torxakis-admin:$GITHUB_TOKEN@github.com/TorXakis/TorXakis.git"
